9617f55a718f0b69601a261cd9bb3cc95f8d4a55
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-01-25 12:59:12 +0530

    Adding Empwage 2nd Usecase

---
+++ Uc2.java
@@ -0,0 +1,15 @@
+public class Uc2 {
+
+	public static void main (String [] a){
+
+		int wagePerHr=20;
+
+		int dailyHr=8;
+
+		int empDailyWage=(wagePerHr * dailyHr );
+
+		System.out.println("Employee Daily Wage=" +empDailyWage);
+	}
+
+}
+
